03aa211d8a23574d4f0c61096e4ca34af2edbc71
novoneelsaha <novoneels@protonmail.com>
2021-12-27 18:00:38 +0600

    -update icon

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 pygame.init()
 # creating the title and icon
 pygame.display.set_caption("Alien Invasion!!!")
+icon = pygame.image.load("rocket.png")
+pygame.display.set_icon(icon)
 # creating the screen
 screen = pygame.display.set_mode((800, 600))
 # loop
