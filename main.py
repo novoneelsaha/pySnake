cf23ec99c14d3e809f98d1b094e0278e22cd5bd9
novoneelsaha <novoneels@protonmail.com>
2021-12-27 17:47:03 +0600

    -made screen -made caption -made game loop

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import pygame
 
 # initialize pygame
 pygame.init()
+# creating the title and icon
+pygame.display.set_caption("Alien Invasion!!!")
+# creating the screen
+screen = pygame.display.set_mode((800, 600))
+# loop
+running = True
+while running == True :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
