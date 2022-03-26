e35c1e0f4cb108f4e288b4f84b82d46f5d0c8f7f
novoneelsaha <novoneels@protonmail.com>
2022-03-26 16:30:05 +0600

    -update spaceship icon

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ pygame.init()
 pygame.display.set_caption("Alien Invasion!!!")
 icon = pygame.image.load("images/rocket.png")
 pygame.display.set_icon(icon)
+# Player
+playerImg = pygame.image.load("images/spaceship.png")
+# x-coordinate of the player
+playerX = 370
+# y-coordinate of the player
+playerY = 480
+# function to draw the player on the screen
+def player():
+    screen.blit(playerImg, (playerX, playerY))
 # creating the screen
 screen = pygame.display.set_mode((800, 600))
 # loop
@@ -14,3 +23,6 @@ while running == True :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            screen.fill((0, 0, 0))
+player()
+pygame.display.update ()
